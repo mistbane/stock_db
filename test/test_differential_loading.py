e6c075d1e529a25a63c495866d20efa114da3d21
mistbane <mistbane@gmail.com>
2020-03-10 12:50:45 -0700

    Remove last record because last day data might be incorrect.

--- test/test_differential_loading.py
+++ test/test_differential_loading.py
@@ -13,9 +13,10 @@ print(df)
 
 #----------------------------------------- test begin --------------
 df_len = len(df.index)
-adf,rec =dfl.differential_loading(sym, df,)
+adf,rec =dfl.differential_loading_to_db(sym, df,)
 # print(adf)
 print(f"Before update: totla {df_len}")
 print(f"After update: Total {len(adf.index)}")
 print(f"rec= {rec}") 
+print(adf.tail(20))
 # print(f"adf = {rec['SPY']['dataset']}") #  NOTE don't forget to use sym to access data.
